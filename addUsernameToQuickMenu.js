610db5307d7e11eb1c3eb73a523921e608902e2d
kem-a <ai.iens@icloud.com>
2024-10-20 23:58:59 +0300

    alpha, added animations to locIcons and Username

--- addUsernameToQuickMenu.js
+++ addUsernameToQuickMenu.js
@@ -6,24 +6,83 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import GObject from 'gi://GObject';
 
 export const AddUsernameToQuickMenu = GObject.registerClass(
-class AddUsernameToQuickMenu extends St.BoxLayout {
+class AddUsernameToQuickMenu extends St.Widget {
     _init() {
         super._init({
-            vertical: false,
+            layout_manager: new Clutter.BinLayout(),
             x_expand: false,
             y_expand: false,
+            clip_to_allocation: true, // Ensure clipping
             style_class: 'username-container',
         });
 
-        const usernameLabel = new St.Label({
+        this._usernameLabel = new St.Label({
             text: GLib.get_real_name() + '  ',
             y_align: Clutter.ActorAlign.CENTER,
-            x_align: Clutter.ActorAlign.START,
+            x_align: Clutter.ActorAlign.CENTER,
             x_expand: false,
             y_expand: false,
             style_class: 'username-label',
         });
-        this.add_child(usernameLabel);
+        this.add_child(this._usernameLabel);
+
+        // Initially hide the actor
+        this.visible = false;
+
+        // Start with the label translated fully to the right
+        this._usernameLabel.translation_x = this._usernameLabel.width;
+    }
+
+    animateIn() {
+        // Ensure the actor is visible
+        this.visible = true;
+
+        // Wait for the actor to be allocated to get its width
+        GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+            // Get the full width of the label
+            let [, naturalWidth] = this._usernameLabel.get_preferred_width(-1);
+
+            // Set the container's width to the label's width
+            this.set_width(naturalWidth);
+
+            // Start the label off-screen to the right
+            this._usernameLabel.translation_x = naturalWidth;
+
+            // Animate the label sliding in from right to left
+            this._usernameLabel.ease({
+                translation_x: 0,
+                duration: 250, // Set duration to 1000 milliseconds
+                mode: Clutter.AnimationMode.LINEAR, // Constant speed
+                onComplete: () => {
+                    this._usernameLabel.translation_x = 0;
+                },
+            });
+
+            return GLib.SOURCE_REMOVE; // Stop the idle callback
+        });
+    }
+
+    animateOut(callback) {
+        // Get the full width of the label
+        let [, naturalWidth] = this._usernameLabel.get_preferred_width(-1);
+
+        // Stop any existing animations
+        this._usernameLabel.remove_all_transitions();
+
+        // Animate the label sliding out to the right
+        this._usernameLabel.ease({
+            translation_x: naturalWidth,
+            duration: 250, // Set duration to 1000 milliseconds
+            mode: Clutter.AnimationMode.LINEAR, // Constant speed
+            onComplete: () => {
+                this._usernameLabel.translation_x = naturalWidth;
+                this.visible = false;
+
+                if (callback) {
+                    callback();
+                }
+            },
+        });
     }
 
     destroy() {
@@ -38,16 +97,22 @@ export function enable() {
         addUsernameInstance = new AddUsernameToQuickMenu();
         const QuickSettingsMenu = Main.panel.statusArea.quickSettings;
 
-        // Insert your instance into the indicators container
+        // Insert your instance into the indicators container at the first position
         QuickSettingsMenu._indicators.insert_child_at_index(addUsernameInstance, 0);
+
+        // Animate the username sliding in
+        addUsernameInstance.animateIn();
     }
 }
 
 export function disable() {
     if (addUsernameInstance) {
-        const QuickSettingsMenu = Main.panel.statusArea.quickSettings;
-        QuickSettingsMenu._indicators.remove_child(addUsernameInstance);
-        addUsernameInstance.destroy();
-        addUsernameInstance = null;
+        // Animate the username sliding out, then remove it
+        addUsernameInstance.animateOut(() => {
+            const QuickSettingsMenu = Main.panel.statusArea.quickSettings;
+            QuickSettingsMenu._indicators.remove_child(addUsernameInstance);
+            addUsernameInstance.destroy();
+            addUsernameInstance = null;
+        });
     }
 }
--- lockIcon.js
+++ lockIcon.js
@@ -1,6 +1,7 @@
 // lockIcon.js
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
+import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
@@ -22,21 +23,68 @@ class LockIcon extends PanelMenu.Button {
             style_class: 'system-status-icon',
         });
 
-        this._capsLockIcon.visible = false;
-        this._numLockIcon.visible = false;
+        // Create individual containers for each icon to handle animations separately
+        this._capsLockContainer = new St.Widget({
+            layout_manager: new Clutter.BinLayout(),
+            x_expand: false,
+            y_expand: false,
+            clip_to_allocation: true, // Ensure clipping
+        });
+        this._capsLockContainer.add_child(this._capsLockIcon);
+
+        this._numLockContainer = new St.Widget({
+            layout_manager: new Clutter.BinLayout(),
+            x_expand: false,
+            y_expand: false,
+            clip_to_allocation: true, // Ensure clipping
+        });
+        this._numLockContainer.add_child(this._numLockIcon);
 
         const layoutManager = new St.BoxLayout({
             vertical: false,
             style_class: 'lockkeys-container',
         });
-        layoutManager.add_child(this._capsLockIcon);
-        layoutManager.add_child(this._numLockIcon);
+        layoutManager.add_child(this._capsLockContainer);
+        layoutManager.add_child(this._numLockContainer);
         this.add_child(layoutManager);
 
-        this._updateLockState();
+        // Initialize the lock states
+        this._capsLockEnabled = this.keymap.get_caps_lock_state();
+        this._numLockEnabled = this.keymap.get_num_lock_state();
 
+        // Set initial properties for the icons
+        this._initializeIcon(this._capsLockContainer, this._capsLockIcon, this._capsLockEnabled);
+        this._initializeIcon(this._numLockContainer, this._numLockIcon, this._numLockEnabled);
+
+        // Connect to the keymap state-changed signal
         this._keymapChangedId = this.keymap.connect('state-changed', () => {
             this._updateLockState();
+        });
+    }
+
+    _initializeIcon(container, icon, enabled) {
+        // Set container visibility immediately
+        container.visible = enabled;
+
+        // Set initial translation_x based on the enabled state
+        icon.translation_x = enabled ? 0 : icon.width;
+
+        // Wait for the actor to be allocated to get its width
+        GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+            // Get the natural width of the icon
+            let [, naturalWidth] = icon.get_preferred_width(-1);
+
+            // Set the container's width to the icon's width
+            container.set_width(naturalWidth);
+
+            // Adjust translation_x if necessary
+            if (enabled) {
+                icon.translation_x = 0;
+            } else {
+                icon.translation_x = naturalWidth;
+            }
+
+            return GLib.SOURCE_REMOVE; // Remove the idle callback
         });
     }
 
@@ -44,8 +92,58 @@ class LockIcon extends PanelMenu.Button {
         const capsLockEnabled = this.keymap.get_caps_lock_state();
         const numLockEnabled = this.keymap.get_num_lock_state();
 
-        this._capsLockIcon.visible = capsLockEnabled;
-        this._numLockIcon.visible = numLockEnabled;
+        if (capsLockEnabled !== this._capsLockEnabled) {
+            this._animateIcon(this._capsLockContainer, this._capsLockIcon, capsLockEnabled);
+            this._capsLockEnabled = capsLockEnabled;
+        }
+        if (numLockEnabled !== this._numLockEnabled) {
+            this._animateIcon(this._numLockContainer, this._numLockIcon, numLockEnabled);
+            this._numLockEnabled = numLockEnabled;
+        }
+    }
+
+    _animateIcon(container, icon, show) {
+        // Stop any existing animations
+        icon.remove_all_transitions();
+
+        // Wait for the actor to be allocated to get its width
+        GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+            // Get the natural width of the icon
+            let [, naturalWidth] = icon.get_preferred_width(-1);
+
+            // Set the container's width to the icon's width
+            container.set_width(naturalWidth);
+
+            if (show) {
+                // Ensure the container is visible
+                container.visible = true;
+
+                // Start the icon off-screen to the right
+                icon.translation_x = naturalWidth;
+
+                // Animate the icon sliding in from right to left
+                icon.ease({
+                    translation_x: 0,
+                    duration: 250, // Duration remains 250 milliseconds
+                    mode: Clutter.AnimationMode.LINEAR,
+                    onComplete: () => {
+                        icon.translation_x = 0;
+                    },
+                });
+            } else {
+                // Animate the icon sliding out to the right
+                icon.ease({
+                    translation_x: naturalWidth,
+                    duration: 250, // Duration remains 250 milliseconds
+                    mode: Clutter.AnimationMode.LINEAR,
+                    onComplete: () => {
+                        icon.translation_x = naturalWidth;
+                        container.visible = false;
+                    },
+                });
+            }
+            return GLib.SOURCE_REMOVE; // Remove the idle callback
+        });
     }
 
     destroy() {
@@ -60,8 +158,10 @@ class LockIcon extends PanelMenu.Button {
 let lockIcon;
 
 export function enable() {
-    lockIcon = new LockIcon();
-    Main.panel.addToStatusArea('lock-indicator', lockIcon, 1, 'right');
+    if (!lockIcon) {
+        lockIcon = new LockIcon();
+        Main.panel.addToStatusArea('lock-indicator', lockIcon, 1, 'right');
+    }
 }
 
 export function disable() {
